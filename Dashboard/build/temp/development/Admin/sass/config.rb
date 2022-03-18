42c5573954d5465a378e37307d2d75bd147cbc98
김 민수(Min-Su Kim) <kimxminsu@gmail.com>
2022-03-18 10:05:39 +0900

    create dashboard project
    - [cmd] sencha -sdk (1.ExtJS gpl 압축 푼 경로) generate app -s (2.ExtJS templates\admin-dashboard 경로) (3.프로젝트 이름) (4.프로젝트 생성할 폴더 경로)
    1. C:\Users\MS.Kim\Downloads\program\ext-7.0.0-gpl\ext-7.0.0
    2. C:\Users\MS.Kim\Downloads\program\ext-7.0.0-gpl\ext-7.0.0\templates\admin-dashboard
    3. Dashboard
    4. C:\Users\MS.Kim\Documents\git\start-extjs\Dashboard

---
+++ Dashboard/build/temp/development/Admin/sass/config.rb
@@ -0,0 +1,1 @@
+cache_path = 'C:\Users\MS.Kim\Documents\git\start-extjs\Dashboard/build/.sass-cache'
---
+++ Dashboard/build/temp/development/Admin/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\Users\MS.Kim\Documents\git\start-extjs\Dashboard\app.js
